48b02b4187eab0b67f9ea4403caa9903165fe58e
pprasitanond <pprasitanond1@babson.edu>
2020-10-10 14:49:03 +0700

    pseudocode initial commit

---
+++ pseudocode.py
@@ -0,0 +1,31 @@
+#SQL Database will be used to hold and store the notes
+#Flask will be used to build web app
+#Tkinter will be used for the interface development
+
+"""Main Features
+    1. Add Notes
+    2. Notes can hold code
+    3. Edit Notes
+    4. Sort Notes by recents (ID), and alphabetical order
+    5. Search Notes from the database
+     """
+
+
+#Importing all of the dependencies
+    #SQL dependencies will be the python extension pymysql
+
+#Connecting the note to the database to store notes
+
+#Create a function connect to a button which allow the user to create  new notes
+
+#Note pad should have the option to hold codes.
+
+#Create a function and connect to a button which allow the user to edit the previous notes.
+
+#Create a search bar for the users to search the title of their notes
+
+
+#Resources
+#https://www.youtube.com/watch?v=4iYquLKgqMU&ab_channel=AlexanderPutilin
+#https://charlesleifer.com/blog/saturday-morning-hack-a-little-note-taking-app-with-flask/
+#https://github.com/ShubhamPy/Note-Taking-App
